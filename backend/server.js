7fad7c0d6d099f3b91284fd6203f72428ebd980b
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-17 10:18:52 +0530

    feat: setup express server and start running on port

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
