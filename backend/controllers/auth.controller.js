bdb8421cb2921c6824625b788f0897cfe0df9b21
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-18 22:49:08 +0530

    Add user registration functionality

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,43 @@
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required.",
+    });
+  }
+
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    return res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({
+      message:
+        error.message ||
+        "Something went wrong while registering the user. Please try after sometime.",
+    });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ import dotenv from "dotenv";
 import express from "express";
 
 import { connectDB } from "./db/index.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 connectDB()
   .then(() => {
