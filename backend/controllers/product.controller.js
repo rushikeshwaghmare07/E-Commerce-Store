6f7e0b9255b342f3b7c11e1b571a2445e9a2f84c
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-25 23:40:02 +0530

    Add delete product  functionality

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -123,3 +123,52 @@ export const getFeaturedProducts = async (req, res) => {
     });
   }
 };
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "Product ID is required.",
+      });
+    }
+
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found.",
+      });
+    }
+
+    // Delete image from Cloudinary
+    if (product.image) {
+      const publicId = product.image?.split("/")?.pop()?.split(".")?.[0];
+      if (publicId) {
+        try {
+          await cloudinary.uploader.destroy(`products/${publicId}`);
+          console.log("Image deleted from Cloudinary");
+        } catch (error) {
+          console.log(
+            "Error in deleting image from Cloudinary:",
+            error.message
+          );
+        }
+      }
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Product deleted successfully.",
+    });
+  } catch (error) {
+    console.log("Error in deleteProduct controller:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProduct, getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
+import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/", protectRoute, adminRoute, createProduct);
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured",  getFeaturedProducts);
+router.delete("/:id", protectRoute, adminRoute ,deleteProduct);
 
 export default router;
