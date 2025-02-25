567f359ff5cbc32e3de19abb8bf899b3f2cdc4f0
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-25 23:20:20 +0530

    Add featured product endpoint

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,5 +1,7 @@
+import { json } from "express";
 import Product from "../models/product.model.js";
 import cloudinary from "../utils/cloudinary.js";
+import redis from "../utils/redis.js";
 
 export const createProduct = async (req, res) => {
   try {
@@ -60,17 +62,60 @@ export const getAllProducts = async (req, res) => {
     if (products.length === 0) {
       return res.status(404).json({
         success: false,
-        message: "No products available."
-      })
+        message: "No products available.",
+      });
     }
 
     return res.status(200).json({
       success: true,
       message: "All products retrieved successfully.",
-      products,
-    })
+      products: products,
+    });
   } catch (error) {
     console.log("Error in getAllProduct controller:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.status(200).json({
+        success: true,
+        message: "Featured products retrieved from cache.",
+        products: JSON.parse(featuredProducts),
+      });
+    }
+
+    // Fetch from database if not cached
+    featuredProducts = await Product.find({ isFeatured: true }).lean(); // lean is used to convert the mongoose object to a plain javascript object
+    if (featuredProducts.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No featured products found.",
+      });
+    }
+
+    // Store in Redis with expiration
+    await redis.set(
+      "featured_products",
+      JSON.stringify(featuredProducts),
+      "EX",
+      3600
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Featured products retrieved successfully.",
+      products: featuredProducts,
+    });
+  } catch (error) {
+    console.log("Error in getFeaturedProducts controller:", error.message);
     return res.status(500).json({
       success: false,
       message: "Internal Server Error",
