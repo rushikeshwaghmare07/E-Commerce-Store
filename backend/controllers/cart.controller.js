462c8e92d197c7be0113e54f89b83695a7a5a6df
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-03-01 23:24:32 +0530

    Implement remove from cart functionality

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -39,18 +39,49 @@ export const addToCart = async (req, res) => {
     }
 
     await user.save();
-    
+
     return res.status(200).json({
       success: true,
       message: "Product added to cart successfully",
       cartItems: user.cartItems,
     });
   } catch (error) {
-    console.log("Error in addToCart controller:", error.message);    
+    console.log("Error in addToCart controller:", error.message);
     return res.status(500).json({
       success: false,
       message: "Internal Server Error",
       error: error.message,
-    })
+    });
+  }
+};
+
+export const removeFromCart = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const user = req.user;
+
+    if (!productId) {
+      return res.status(400).json({
+        success: false,
+        message: "Product ID is required",
+      });
+    }
+
+    user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Product removed from cart successfully",
+      cartItems: user.cartItems,
+    });
+  } catch (error) {
+    console.log("Error in removeFromCart controller:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
   }
 };
--- backend/routes/cart.routes.js
+++ backend/routes/cart.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware";
-import { getCartProducts, addToCart } from "../controllers/cart.controller.js";
+import { getCartProducts, addToCart, removeAllFromCart } from "../controllers/cart.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getCartProducts);
 router.post("/", protectRoute, addToCart);
+router.delete("/", protectRoute, removeAllFromCart);
 
 export default router;
