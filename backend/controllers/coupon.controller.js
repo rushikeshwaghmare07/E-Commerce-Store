162c7eeabb4e0e2011454d6dd2d9545a4b2aa2ea
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-03-03 23:42:22 +0530

    Implement Validate Coupon functionality

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -28,3 +28,45 @@ export const getCoupon = async (req, res) => {
     });
   }
 };
+
+export const validateCoupon = async (req, res) => {
+  try {
+    const { code } = req.body;
+
+    const coupon = await Coupon.findOne({
+      code,
+      userId: req.user._id,
+      isActive: true,
+    });
+
+    if (!coupon) {
+      return res.status(404).json({
+        success: false,
+        message: "Coupon not found",
+      });
+    }
+
+    if (coupon.expirationDate < new Date()) {
+      coupon.isActive = false;
+      await coupon.save();
+      return res.status(400).json({
+        success: false,
+        message: "Coupon has expired",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Coupon is valid",
+      code: coupon.code,
+      discountPercentage: coupon.discountPercentage,
+    });
+  } catch (error) {
+    console.log("Error in validateCoupon controller:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
